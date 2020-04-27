c34e4e1d1de700567032cca960984da5d2c2433c
mariusshark <62962314+mariusshark@users.noreply.github.com>
2020-04-27 15:53:14 +0200

    pridetas index 0

---
+++ php/index03.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="lt">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php02</title>
+</head>
+
+<body>
+    <?php
+    $x = 10;
+    $y = 7;
+  
+    ?>
+    <?php $z = $x+$y; echo $x . '+' . $y . '=' . $z;
+    ?>
+<br>
+    <?php $z = $x-$y; echo $x . '-' . $y . '=' . $z;
+    ?>
+<br>
+    <?php $z = $x*$y; echo $x . '*' . $y . '=' . $z;
+    ?>
+<br>
+    <?php $z = $x/$y; echo $x . '/' . $y . '=' . $z;
+    ?>
+<br>
+    <?php $z = $x%$y; echo $x . '%' . $y . '=' . $z;
+    ?>
+</body>
+
+</html>
